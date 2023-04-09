e67b0a9743550271f00c91c528a55be48831eb29
d <d.mirauta@protonmail.com>
2023-04-09 11:48:43 +0100

    structure

--- day 7/sol.py
+++ day 7/sol.py
@@ -1,32 +1,10 @@
 import re
 
-with open("input7", "r") as f:
+with open("input", "r") as f:
     lines = f.readlines()
 
-test_lines = """$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
-$ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k""".split("\n")
-
+with open("test_input", "r") as f:
+    test_lines = f.readlines()
 
 class Dir:
 
