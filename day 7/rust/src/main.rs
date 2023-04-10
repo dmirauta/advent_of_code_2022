8b109dc44c16c280eb4914cb203709b87aa7e0e2
D <d.mirauta@protonmail.com>
2023-04-10 19:52:37 +0100

    prior to embeding structs

--- day 7/rust/src/main.rs
+++ day 7/rust/src/main.rs
@@ -1,13 +1,48 @@
 
+#[derive(Debug)]
 struct File {
     name: String,
     size: usize
 }
 
+#[derive(Debug)]
 struct Dir {
     name: String,
     parent: Option<Box<Dir>>,
     children: Vec<Box<FSNode>>
+}
+
+#[derive(Debug)]
+enum FSNode {
+    File(File),
+    Dir(Dir)
+}
+
+#[derive(Debug)]
+struct DirTree {
+    root: FSNode,
+    current_dir: Box<FSNode>,
+}
+
+trait Size {
+    fn get_size(&self) -> usize;
+}
+
+impl Size for File {
+    fn get_size(&self) -> usize {
+        self.size
+    }
+}
+
+impl Size for Dir {
+    fn get_size(&self) -> usize {
+        self.children.iter().map(|c| {
+            match c.as_ref() {
+                FSNode::File(file) => file.get_size(),
+                FSNode::Dir(dir) => dir.get_size(),
+            }
+        }).sum()
+    }
 }
 
 impl Dir {
@@ -27,22 +62,22 @@ impl Dir {
         }
         println!("");
     }
-}
 
-enum FSNode {
-    File(File),
-    Dir(Dir)
-}
-
-struct DirTree {
-    root: FSNode,
-    current_dir: Box<FSNode>,
+    fn subdirs(&self) -> Vec<&Box<FSNode>> {
+        self.children.iter().filter(|c| {
+            if let FSNode::Dir(dir) = c.as_ref() {true} else {false}
+        }).collect()
+    }
 }
 
 fn main() {
     let mut root = Dir {name:"/".to_string(), parent:None, children:vec![]};
     root.add_file(File {name:"a".to_string(), size:50});
-    root.add_file(File {name:"b".to_string(), size:60});
+    root.add_dir(Dir {name:"b".to_string(), parent:None, children:vec![]});
 
     root.ls();
+
+    for c in root.subdirs() {
+        dbg!(c);
+    }
 }
