ab105a7ad1df7905968f9174ecbf7babbb1b10b3
d <d.mirauta@protonmail.com>
2023-04-10 16:08:13 +0100

    basic structs

--- day 7/rust/src/main.rs
+++ day 7/rust/src/main.rs
@@ -1,3 +1,48 @@
+
+struct File {
+    name: String,
+    size: usize
+}
+
+struct Dir {
+    name: String,
+    parent: Option<Box<Dir>>,
+    children: Vec<Box<FSNode>>
+}
+
+impl Dir {
+    fn add_file(&mut self, child: File) {
+        self.children.push( Box::new(FSNode::File(child)) );
+    }
+    fn add_dir(&mut self, child: Dir) {
+        self.children.push( Box::new(FSNode::Dir(child)) );
+    }
+    fn ls(&self) {
+        println!("ls {}", self.name);
+        for fsn in self.children.iter() {
+            match fsn.as_ref() {
+                FSNode::File(file) => println!("{} {}", file.name, file.size),
+                FSNode::Dir(dir) => println!("{} {}", dir.name, dir.children.len()),
+            }
+        }
+        println!("");
+    }
+}
+
+enum FSNode {
+    File(File),
+    Dir(Dir)
+}
+
+struct DirTree {
+    root: FSNode,
+    current_dir: Box<FSNode>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut root = Dir {name:"/".to_string(), parent:None, children:vec![]};
+    root.add_file(File {name:"a".to_string(), size:50});
+    root.add_file(File {name:"b".to_string(), size:60});
+
+    root.ls();
 }
