9a1cdf401f3da2002c2196572c43f1314272c49f
d <d.mirauta@protonmail.com>
2023-05-21 23:43:42 +0100

    working part 1 with breadth first floodfill

--- day 12/rust/src/main.rs
+++ day 12/rust/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Display, fs};
+use std::{collections::VecDeque, fmt::Display, fs};
 
 #[derive(Debug)]
 enum Space {
@@ -22,26 +22,38 @@ impl Space {
 }
 
 type Pos = (usize, usize);
-type Row = Vec<Space>;
+#[derive(Clone)]
+struct ApproachDescriptor {
+    pos: Option<Pos>,
+    dist: usize,
+}
 
+impl ApproachDescriptor {
+    fn new() -> Self {
+        ApproachDescriptor { pos: None, dist: 0 }
+    }
+}
+
+type Row = Vec<Space>;
 struct Grid {
     spaces: Vec<Row>,
+    best_approach: Vec<Vec<ApproachDescriptor>>,
     height: usize,
     width: usize,
     start: Pos,
+    end: Pos,
 }
 
 #[derive(Clone)]
 struct Path {
     spaces: Vec<Pos>,
-    length: usize,
 }
 
 impl Path {
     fn new(start: Pos) -> Self {
         let mut spaces = Vec::with_capacity(100);
         spaces.push(start);
-        Path { spaces, length: 1 }
+        Path { spaces }
     }
 
     fn contains(&self, p: &Pos) -> bool {
@@ -74,14 +86,14 @@ enum Direction {
 fn dir((fi, fj): Pos, (ti, tj): Pos) -> Direction {
     if ti == fi + 1 {
         Direction::Down
-    } else if ti == fi - 1 {
+    } else if ti + 1 == fi {
         Direction::Up
     } else if tj == fj + 1 {
         Direction::Right
-    } else if tj == fj - 1 {
+    } else if tj + 1 == fj {
         Direction::Left
     } else {
-        panic!();
+        panic!("Unexpected jump");
     }
 }
 
@@ -98,6 +110,7 @@ impl Grid {
     fn parse(contents: String) -> Self {
         let mut spaces: Vec<Row> = vec![];
         let mut start: Pos = (0, 0);
+        let mut end: Pos = (0, 0);
         for (i, raw_row) in contents.lines().enumerate() {
             let mut row: Row = vec![];
             for (j, c) in raw_row.chars().enumerate() {
@@ -106,6 +119,7 @@ impl Grid {
                     start = (i, j);
                 } else if c == 'E' {
                     row.push(Space::End);
+                    end = (i, j);
                 } else {
                     row.push(Space::Mid(c));
                 }
@@ -118,72 +132,20 @@ impl Grid {
 
         Grid {
             spaces,
+            best_approach: vec![vec![ApproachDescriptor::new(); width]; height],
             height,
             width,
             start,
+            end,
         }
     }
 
-    fn extend(&self, path: &Path, dir: Direction, max_len: usize) -> Option<Path> {
-        let mut new_path = path.clone();
-        let (endi, endj) = *path.spaces.last().expect("Path should not be empty");
+    fn floodfill(&mut self) {
+        self.best_approach[self.start.0][self.start.1].pos = Some(self.start);
+        let mut queue: VecDeque<Pos> = VecDeque::from([self.start]);
 
-        let opt_pos = match dir {
-            Direction::Left => {
-                if endi > 0 {
-                    Some((endi - 1, endj))
-                } else {
-                    None
-                }
-            }
-            Direction::Right => {
-                if endi < self.height - 1 {
-                    Some((endi + 1, endj))
-                } else {
-                    None
-                }
-            }
-            Direction::Up => {
-                if endj > 0 {
-                    Some((endi, endj - 1))
-                } else {
-                    None
-                }
-            }
-            Direction::Down => {
-                if endj < self.width - 1 {
-                    Some((endi, endj + 1))
-                } else {
-                    None
-                }
-            }
-        };
-
-        if let Some((i, j)) = opt_pos {
-            if self.spaces[endi][endj].can_reach(&self.spaces[i][j])
-                && !new_path.contains(&(i, j))
-                && new_path.length < max_len
-            {
-                new_path.spaces.push((i, j));
-                new_path.length += 1;
-                return Some(new_path);
-            }
-        }
-        None
-    }
-
-    fn search(&self, max_iter: usize, max_path_size: usize) -> Option<Path> {
-        let mut paths: Vec<Path> = Vec::with_capacity(100);
-        paths.push(Path::new(self.start));
-        let mut shortest: Option<Path> = None;
-        let mut max_size: usize = max_path_size;
-
-        let mut iters = 0;
-        while let Some(path) = paths.pop() {
-            if iters >= max_iter {
-                break;
-            }
-            iters += 1;
+        while let Some((i, j)) = queue.pop_front() {
+            let from_dist = self.best_approach[i][j].dist;
 
             for dir in [
                 Direction::Left,
@@ -191,39 +153,83 @@ impl Grid {
                 Direction::Up,
                 Direction::Down,
             ] {
-                if let Some(new_path) = self.extend(&path, dir, max_size) {
-                    // println!("{}", new_path);
-                    let (i, j) = new_path.spaces.last().unwrap().clone();
-                    if let Space::End = self.spaces[i][j] {
-                        let sp = match shortest {
-                            None => new_path.clone(),
-                            Some(sp) => {
-                                if sp.length > new_path.length {
-                                    new_path.clone()
-                                } else {
-                                    sp
-                                }
-                            }
-                        };
-                        max_size = sp.length;
-                        shortest = Some(sp);
-                    } else {
-                        paths.push(new_path);
+                if let Some((ni, nj)) = self.try_march((i, j), dir) {
+                    if let None = self.best_approach[ni][nj].pos {
+                        self.best_approach[ni][nj].pos = Some((i, j));
+                        self.best_approach[ni][nj].dist = from_dist + 1;
+                        queue.push_back((ni, nj));
                     }
                 }
             }
+        }
+    }
 
-            if iters % 1_000_000 == 0 {
-                dbg!(paths.len());
+    fn try_march(&self, (i, j): Pos, dir: Direction) -> Option<Pos> {
+        let opt_pos = match dir {
+            Direction::Left => {
+                if i > 0 {
+                    Some((i - 1, j))
+                } else {
+                    None
+                }
+            }
+            Direction::Right => {
+                if i < self.height - 1 {
+                    Some((i + 1, j))
+                } else {
+                    None
+                }
+            }
+            Direction::Up => {
+                if j > 0 {
+                    Some((i, j - 1))
+                } else {
+                    None
+                }
+            }
+            Direction::Down => {
+                if j < self.width - 1 {
+                    Some((i, j + 1))
+                } else {
+                    None
+                }
+            }
+        };
+
+        if let Some((ni, nj)) = opt_pos {
+            if self.spaces[i][j].can_reach(&self.spaces[ni][nj]) {
+                return Some((ni, nj));
             }
         }
+        None
+    }
 
-        if iters == max_iter {
-            self.vis_path(&paths.last().unwrap());
-            self.path_elevation(&paths.last().unwrap());
+    fn backtrace(&self) -> Path {
+        let mut shortest: Path = Path::new(self.end);
+        loop {
+            let (i, j) = *shortest.spaces.last().unwrap();
+            let prev = self.best_approach[i][j].pos.unwrap();
+            shortest.spaces.push(prev);
+            if prev == self.start {
+                break;
+            }
         }
-        dbg!(iters);
+        shortest.spaces.reverse();
         shortest
+    }
+
+    fn vis_approach(&self) {
+        for i in 0..self.height {
+            for j in 0..self.width {
+                let pd = self.best_approach[i][j].dist;
+                if let Some((pi, pj)) = self.best_approach[i][j].pos {
+                    print!("({} {}| {}) ", pi, pj, pd);
+                } else {
+                    print!("(None| {}) ", pd);
+                }
+            }
+            println!();
+        }
     }
 
     fn vis_path(&self, path: &Path) {
@@ -231,7 +237,7 @@ impl Grid {
             for j in 0..self.width {
                 let mut c = '.';
                 if let Some(k) = path.find(&(i, j)) {
-                    if k < path.length - 1 {
+                    if k < path.spaces.len() - 1 {
                         c = char_dir(path.spaces[k], path.spaces[k + 1]);
                     }
                 }
@@ -262,14 +268,15 @@ fn main() {
     // let contents = fs::read_to_string(TEST_INPUT_PATH).expect("Could not read {TEST_INPUT_PATH}");
     let contents = fs::read_to_string(INPUT_PATH).expect("Could not read {INPUT_PATH}");
 
-    let grid = Grid::parse(contents);
-    println!("{}", grid.height);
-    println!("{}", grid.width);
+    let mut grid = Grid::parse(contents);
+    // println!("{}", grid.height);
+    // println!("{}", grid.width);
 
-    if let Some(min_path) = grid.search(10_000_000, 500) {
-        println!("{}", min_path);
-        println!("{}", min_path.length - 1);
+    grid.floodfill();
 
-        grid.vis_path(&min_path);
-    }
+    // grid.vis_approach();
+
+    let shortest = grid.backtrace();
+    grid.vis_path(&shortest);
+    dbg!(shortest.spaces.len() - 1);
 }
