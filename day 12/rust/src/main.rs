85f7b6abe552653bed344cfc7211048914b2ea5c
d <d.mirauta@protonmail.com>
2023-05-23 21:23:52 +0100

    part 2 done

--- day 12/rust/src/main.rs
+++ day 12/rust/src/main.rs
@@ -1,3 +1,4 @@
+use colored::Colorize;
 use std::{collections::VecDeque, fmt::Display, fs};
 
 #[derive(Debug)]
@@ -107,7 +108,7 @@ fn char_dir(from: Pos, to: Pos) -> char {
 }
 
 impl Grid {
-    fn parse(contents: String) -> Self {
+    fn parse(contents: &String) -> Self {
         let mut spaces: Vec<Row> = vec![];
         let mut start: Pos = (0, 0);
         let mut end: Pos = (0, 0);
@@ -140,9 +141,12 @@ impl Grid {
         }
     }
 
-    fn floodfill(&mut self) {
-        self.best_approach[self.start.0][self.start.1].pos = Some(self.start);
-        let mut queue: VecDeque<Pos> = VecDeque::from([self.start]);
+    fn floodfill<F>(&mut self, start: Pos, move_is_valid: F)
+    where
+        F: Fn(&Space, &Space) -> bool,
+    {
+        self.best_approach[start.0][start.1].pos = Some(start);
+        let mut queue: VecDeque<Pos> = VecDeque::from([start]);
 
         while let Some((i, j)) = queue.pop_front() {
             let from_dist = self.best_approach[i][j].dist;
@@ -153,7 +157,7 @@ impl Grid {
                 Direction::Up,
                 Direction::Down,
             ] {
-                if let Some((ni, nj)) = self.try_march((i, j), dir) {
+                if let Some((ni, nj)) = self.try_march((i, j), dir, &move_is_valid) {
                     if let None = self.best_approach[ni][nj].pos {
                         self.best_approach[ni][nj].pos = Some((i, j));
                         self.best_approach[ni][nj].dist = from_dist + 1;
@@ -164,7 +168,11 @@ impl Grid {
         }
     }
 
-    fn try_march(&self, (i, j): Pos, dir: Direction) -> Option<Pos> {
+    /// hides border checking logic
+    fn try_march<F>(&self, (i, j): Pos, dir: Direction, move_is_valid: &F) -> Option<Pos>
+    where
+        F: Fn(&Space, &Space) -> bool,
+    {
         let opt_pos = match dir {
             Direction::Left => {
                 if i > 0 {
@@ -197,22 +205,24 @@ impl Grid {
         };
 
         if let Some((ni, nj)) = opt_pos {
-            if self.spaces[i][j].can_reach(&self.spaces[ni][nj]) {
+            if move_is_valid(&self.spaces[i][j], &self.spaces[ni][nj]) {
                 return Some((ni, nj));
             }
         }
         None
     }
 
-    fn backtrace(&self) -> Path {
-        let mut shortest: Path = Path::new(self.end);
+    /// Backtraces from given position back to floodfill start
+    fn backtrace(&self, pos: Pos) -> Path {
+        let mut shortest: Path = Path::new(pos);
         loop {
             let (i, j) = *shortest.spaces.last().unwrap();
             let prev = self.best_approach[i][j].pos.unwrap();
-            shortest.spaces.push(prev);
-            if prev == self.start {
+            // starting flood fill square lists itself as best approach
+            if prev == (i, j) {
                 break;
             }
+            shortest.spaces.push(prev);
         }
         shortest.spaces.reverse();
         shortest
@@ -223,9 +233,42 @@ impl Grid {
             for j in 0..self.width {
                 let pd = self.best_approach[i][j].dist;
                 if let Some((pi, pj)) = self.best_approach[i][j].pos {
-                    print!("({} {}| {}) ", pi, pj, pd);
+                    print!(
+                        "({} | {} {} | {}) ",
+                        self.spaces[i][j].elevation() as char,
+                        pi,
+                        pj,
+                        pd
+                    );
                 } else {
                     print!("(None| {}) ", pd);
+                }
+            }
+            println!();
+        }
+    }
+
+    fn distance_field(&self, opt_path: Option<&Path>) {
+        for i in 0..self.height {
+            for j in 0..self.width {
+                let pd = self.best_approach[i][j].dist;
+                if let Some(_) = self.best_approach[i][j].pos {
+                    let mut t: Option<char> = None;
+                    if let Some(path) = opt_path {
+                        if let Some(k) = path.find(&(i, j)) {
+                            if k < path.spaces.len() - 1 {
+                                t = Some(char_dir(path.spaces[k], path.spaces[k + 1]));
+                            }
+                        }
+                    }
+                    if let Some(ch) = t {
+                        print!("{}", ch);
+                    } else {
+                        let co = (pd / 3) as u8;
+                        print!("{}", "@".truecolor(0, 255 - co, co));
+                    }
+                } else {
+                    print!(" ");
                 }
             }
             println!();
@@ -246,19 +289,52 @@ impl Grid {
             println!("");
         }
     }
+}
 
-    fn path_elevation(&self, path: &Path) {
-        for i in 0..self.height {
-            for j in 0..self.width {
-                let mut c = '.';
-                if path.contains(&(i, j)) {
-                    c = self.spaces[i][j].elevation() as char;
-                }
-                print!("{}", c);
+fn part_1(contents: &String) -> usize {
+    let mut grid = Grid::parse(contents);
+
+    // floodfill calculates steps from start each space
+    grid.floodfill(grid.start, |ss, ds| ss.can_reach(ds));
+
+    // grid.vis_approach();
+
+    let shortest = grid.backtrace(grid.end);
+    grid.vis_path(&shortest);
+    grid.distance_field(Some(&shortest));
+    shortest.spaces.len() - 1
+}
+
+fn part_2(contents: &String) -> usize {
+    let mut grid = Grid::parse(contents);
+
+    // floodfill calculates steps to end from each space
+    grid.floodfill(grid.end, |ss, ds| ds.can_reach(ss));
+
+    // grid.vis_approach();
+
+    let mut min_pos: Pos = (0, 0);
+    let mut min_dist = usize::MAX;
+    for i in 0..grid.height {
+        for j in 0..grid.width {
+            if grid.spaces[i][j].elevation() == b'a'
+                && grid.best_approach[i][j].dist < min_dist
+                && grid.best_approach[i][j].pos.is_some()
+            {
+                min_dist = grid.best_approach[i][j].dist;
+                min_pos = (i, j);
             }
-            println!("");
         }
     }
+
+    dbg!(min_pos);
+    let mut shortest = grid.backtrace(min_pos);
+    shortest.spaces.reverse();
+    grid.vis_path(&shortest);
+
+    grid.distance_field(Some(&shortest));
+
+    min_dist
 }
 
 static TEST_INPUT_PATH: &str = "../test_input";
@@ -268,15 +344,6 @@ fn main() {
     // let contents = fs::read_to_string(TEST_INPUT_PATH).expect("Could not read {TEST_INPUT_PATH}");
     let contents = fs::read_to_string(INPUT_PATH).expect("Could not read {INPUT_PATH}");
 
-    let mut grid = Grid::parse(contents);
-    // println!("{}", grid.height);
-    // println!("{}", grid.width);
-
-    grid.floodfill();
-
-    // grid.vis_approach();
-
-    let shortest = grid.backtrace();
-    grid.vis_path(&shortest);
-    dbg!(shortest.spaces.len() - 1);
+    dbg!(part_1(&contents));
+    dbg!(part_2(&contents));
 }
