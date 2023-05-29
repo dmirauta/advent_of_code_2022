94bd73ef0a7b75deed438c3053311f3a3a41faa6
d <d.mirauta@protonmail.com>
2023-05-29 19:53:21 +0100

    part 1 done

--- day 15/rust/src/main.rs
+++ day 15/rust/src/main.rs
@@ -1,3 +1,89 @@
+use regex::Regex;
+use std::{collections::HashSet, fs};
+
+#[macro_use]
+extern crate lazy_static;
+
+#[derive(Debug, Clone, Copy)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Point {
+    fn lev_dist_to(&self, other: Point) -> i32 {
+        (self.x - other.x).abs() + (self.y - other.y).abs()
+    }
+}
+
+#[derive(Debug)]
+struct SensorData {
+    sensor_pos: Point,
+    beacon_pos: Point,
+    empty_zone_radius: i32,
+}
+
+fn parse(contents: &String) -> Vec<SensorData> {
+    lazy_static! {
+        static ref REG: Regex =
+            Regex::new(r"Sensor at x=(.+), y=(.+): closest beacon is at x=(.+), y=(.+)")
+                .expect("Regex compile failure");
+    }
+
+    let mut data: Vec<SensorData> = vec![];
+
+    for line in contents.lines() {
+        if let Some(cap) = REG.captures(line) {
+            let sensor_pos = Point {
+                x: cap.get(1).unwrap().as_str().parse().unwrap(),
+                y: cap.get(2).unwrap().as_str().parse().unwrap(),
+            };
+            let beacon_pos = Point {
+                x: cap.get(3).unwrap().as_str().parse().unwrap(),
+                y: cap.get(4).unwrap().as_str().parse().unwrap(),
+            };
+            data.push(SensorData {
+                sensor_pos,
+                beacon_pos,
+                empty_zone_radius: sensor_pos.lev_dist_to(beacon_pos),
+            });
+        }
+    }
+
+    data
+}
+
+fn part_1(data: &Vec<SensorData>, row: i32) {
+    let mut free_positions: HashSet<i32> = HashSet::new();
+
+    let mut beacons_in_row = HashSet::new();
+    for d in data {
+        if d.beacon_pos.y == row {
+            beacons_in_row.insert(d.beacon_pos.x);
+        }
+    }
+
+    for d in data {
+        let partial_dist = (row - d.sensor_pos.y).abs(); // distance to sensor projected to row
+        for i in 0..=d.empty_zone_radius - partial_dist {
+            free_positions.insert(d.sensor_pos.x - i);
+            free_positions.insert(d.sensor_pos.x + i);
+        }
+    }
+
+    dbg!(free_positions.len() - beacons_in_row.len());
+}
+
+static TEST_INPUT_PATH: &str = "../test_input";
+static INPUT_PATH: &str = "../input";
+
 fn main() {
-    println!("Hello, world!");
+    let tcontents = fs::read_to_string(TEST_INPUT_PATH).expect("Could not read {TEST_INPUT_PATH}");
+    let contents = fs::read_to_string(INPUT_PATH).expect("Could not read {INPUT_PATH}");
+
+    let tdata = parse(&tcontents);
+    let data = parse(&contents);
+
+    part_1(&tdata, 10);
+    part_1(&data, 2_000_000);
 }
