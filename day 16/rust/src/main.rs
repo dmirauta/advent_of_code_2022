02e470f53a1f2416dbbef70eebaefbf950caff4f
d <d.mirauta@protonmail.com>
2023-07-04 23:44:51 +0100

    consider only paths to nonzero (not yet working)

--- day 16/rust/src/main.rs
+++ day 16/rust/src/main.rs
@@ -1,7 +1,4 @@
-use crossterm::{
-    cursor::{Hide, Show},
-    ExecutableCommand,
-};
+use crossterm::{cursor, ExecutableCommand};
 use regex::Regex;
 use std::{collections::HashMap, fs, io::stdout, time::Instant};
 
@@ -15,8 +12,7 @@ extern crate lazy_static;
 struct Valve<'a> {
     name: &'a str,
     rate: u32,
-    all_connections: Vec<usize>,
-    relevant_connections: Vec<(usize, u32)>,
+    connections: Vec<usize>,
 }
 
 fn parse_valve(line: &str) -> (&str, u32, Vec<&str>) {
@@ -43,6 +39,7 @@ fn parse_valve(line: &str) -> (&str, u32, Vec<&str>) {
 
 struct Valves<'a> {
     all: Vec<Valve<'a>>,
+    major: Vec<usize>,
     ids: HashMap<&'a str, usize>,
     floodfills: HashMap<usize, FloodFill<usize>>,
     num: usize,
@@ -53,34 +50,38 @@ impl<'a> Valves<'a> {
     fn from(contents: &'a String) -> Self {
         let parsed_valves = Vec::from_iter(contents.lines().map(parse_valve));
         let mut all = vec![];
+        let mut major = vec![];
         let num = parsed_valves.len();
 
-        let ids = HashMap::from_iter(
-            parsed_valves
-                .iter()
-                .enumerate()
-                .map(|(i, (name, _, _))| (*name, i)),
-        );
+        let mut ids = HashMap::new();
+
+        for (i, (name, rate, _)) in parsed_valves.iter().enumerate() {
+            ids.insert(*name, i);
+            if *rate > 0 {
+                major.push(i);
+            }
+        }
+
         let start_idx = ids["AA"];
 
         for (name, rate, str_conn) in parsed_valves.iter() {
-            let all_connections: Vec<usize> = Vec::from_iter(str_conn.iter().map(|to| ids[*to]));
+            let connections: Vec<usize> = Vec::from_iter(str_conn.iter().map(|to| ids[*to]));
             all.push(Valve {
                 name: *name,
                 rate: *rate,
-                all_connections,
-                relevant_connections: vec![],
+                connections,
             })
         }
 
-        let edges = (0..num)
-            .map(|i| (i, all[i].all_connections.clone()))
-            .collect();
+        let edges = (0..num).map(|i| (i, all[i].connections.clone())).collect();
 
+        // we actually only need to pathfind from starting room or relevant_connections
+        // mechanism for lazily computing these might be nice
         let floodfills = (0..num).map(|i| (i, FloodFill::new(i, &edges))).collect();
 
         Self {
             all,
+            major,
             ids,
             floodfills,
             num,
@@ -93,6 +94,7 @@ impl<'a> Valves<'a> {
 enum Action {
     Move(usize),
     Open,
+    Stay,
 }
 
 #[derive(Debug, Clone)]
@@ -119,53 +121,47 @@ impl State {
         }
     }
 
-    fn next(&self) -> Self {
+    fn next(&self, actions: Vec<Action>) -> Self {
         let mut new = self.clone();
-        new.minute += 1;
-        new.released_pressure += self.total_rate;
+        for action in actions {
+            new.minute += 1;
+            new.released_pressure += self.total_rate;
+            new.hist.push(action);
+        }
         new
     }
 
-    fn forms_cycle(&self, next_valve: usize) -> bool {
-        if let Some((last_occured, _)) = self.hist.iter().enumerate().rev().find(|(_, a)| match a {
-            Action::Move(valve) => *valve == next_valve,
-            _ => false,
-        }) {
-            if let Some(_) = (last_occured + 1..self.hist.len()).find(|i| match self.hist[*i] {
-                Action::Open => true,
-                _ => false,
-            }) {
-                return false;
-            } else {
-                return true;
-            }
-        }
-        false
-    }
-
     /// Returns possible next actions
-    fn insert_future(&self, valves: &Vec<Valve>, next_actions: &mut Vec<State>) {
+    fn insert_future(&self, valves: &Valves, next_actions: &mut Vec<State>) {
         if self.minute >= 30 {
             return;
         }
 
-        // travel to neighbouring room
-        for &neighbour_idx in valves[self.currently_at].all_connections.iter() {
-            if !self.forms_cycle(neighbour_idx) {
-                let mut new = self.next();
-                new.currently_at = neighbour_idx;
-                new.hist.push(Action::Move(neighbour_idx));
-                next_actions.push(new);
-            }
+        let remaining_major: Vec<&usize> = valves
+            .major
+            .iter()
+            .filter(|ni| !self.opened[**ni])
+            .collect();
+
+        if remaining_major.len() == 0 {
+            next_actions.push(self.next(vec![Action::Stay]));
+            return;
         }
 
-        // open valve in current room
-        if !self.opened[self.currently_at] && valves[self.currently_at].rate != 0 {
-            let mut new = self.next();
-            new.opened[self.currently_at] = true;
-            new.total_rate += valves[self.currently_at].rate;
-            new.hist.push(Action::Open);
-            next_actions.push(new);
+        for &neighbour_idx in remaining_major {
+            let path = valves.floodfills[&self.currently_at].path_to(neighbour_idx);
+            let mut actions: Vec<_> = path[1..].iter().map(|i| Action::Move(*i)).collect();
+            actions.push(Action::Open);
+
+            let mut new = self.next(actions);
+            new.currently_at = neighbour_idx;
+
+            new.opened[neighbour_idx] = true;
+            new.total_rate += valves.all[neighbour_idx].rate;
+
+            if new.minute <= 30 {
+                next_actions.push(new);
+            }
         }
     }
 }
@@ -174,31 +170,31 @@ fn state_string(state: &State, valves: &Vec<Valve>) -> String {
     let mut s = format!("released_pressure: {}\n", state.released_pressure);
     for action in state.hist.iter() {
         match action {
-            Action::Move(id) => s += format!("{}\n", valves[*id].name).as_str(),
+            Action::Move(id) => s += format!("Move to {}\n", valves[*id].name).as_str(),
             Action::Open => s += format!("Open\n").as_str(),
+            Action::Stay => s += format!("Stay\n").as_str(),
         };
     }
     s
 }
 
 fn part1(valves: &Valves) {
-    let mut queue: Vec<State> = vec![State::starting(valves.start_idx, valves.num)];
     let mut best = State::starting(valves.start_idx, valves.num);
+    let mut queue: Vec<State> = vec![best.clone()];
 
-    // brute action tree search
     let mut i: u64 = 0;
-    let maxsecs = 600;
+    let maxsecs = 60;
     let start = Instant::now();
-    stdout().execute(Hide).unwrap();
+    stdout().execute(cursor::Hide).unwrap();
     while let Some(current) = queue.pop() {
-        current.insert_future(&valves.all, &mut queue);
+        current.insert_future(&valves, &mut queue);
 
         if current.released_pressure > best.released_pressure {
             best = current;
         }
 
         i += 1;
-        if i % 10000 == 0 {
+        if i % 100 == 0 {
             print!(
                 "i = {}, time = {}, queue size = {}, best pressure released = {}     \r",
                 &i,
@@ -211,7 +207,7 @@ fn part1(valves: &Valves) {
             break;
         }
     }
-    stdout().execute(Show).unwrap();
+    stdout().execute(cursor::Show).unwrap();
 
     println!("\n{}", state_string(&best, &valves.all));
 
@@ -237,17 +233,21 @@ fn main() {
     let tcontents = fs::read_to_string(TEST_INPUT_PATH).expect("Could not read {TEST_INPUT_PATH}");
     let contents = fs::read_to_string(INPUT_PATH).expect("Could not read {INPUT_PATH}");
 
-    let valves = Valves::from(&tcontents);
+    let valves = Valves::from(&contents);
     // part1(&valves);
 
     let ff_from_aa = &valves.floodfills[&valves.start_idx];
-    let target = valves.ids["HH"];
+    // let target = valves.ids["ZX"];
+    //
+    // dbg!(ff_from_aa
+    //     .path_to(target)
+    //     .iter()
+    //     .map(|&i| valves.all[i].name)
+    //     .collect::<Vec<_>>());
+    //
+    // dbg!(ff_from_aa.dist(target));
 
-    dbg!(ff_from_aa
-        .path_to(target)
-        .iter()
-        .map(|&i| valves.all[i].name)
-        .collect::<Vec<_>>());
-
-    dbg!(ff_from_aa.dist(target));
+    for i in valves.major {
+        dbg!(valves.all[i].name, ff_from_aa.dist(i));
+    }
 }
